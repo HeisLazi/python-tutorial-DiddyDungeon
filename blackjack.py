63f08f0f98ecd7917c41aa232eaed8fad2a985d2
Lazi <lazi11petrus@gmail.com>
2026-09-17 23:06:32 +0200

    sync(workspace): transfer allowlisted project files

--- blackjack.py
+++ blackjack.py
@@ -20,34 +20,47 @@ print(f"My first card is: {dealer_hand[0]}")
 d_score = calculate_score(dealer_hand)
 p_score = calculate_score(player_hand)
 
-player_turn = True
-while player_turn:
-    choice = input(f"\nwhat do yu want to do BOY (hit / stand): ").strip().lower()
-    if choice == "hit":
-        player_hand.append(deal_card())
-        p_score = calculate_score(player_hand)
-        print(f"You ahh got {p_score} LMAOOO")
+play_again = True
 
-        if p_score > 21:
-            print("I JUST BUSTED ALL OVER YOOOOOUUUUUU")
-            player_turn = False
-        elif p_score == 21:
-            print("u gotta be kavango or smth??? i refuse to believe u beat me")
-            player_turn = False
+while play_again:
+    player_hand = [deal_card(), deal_card()]
+    dealer_hand = [deal_card(), deal_card()]
+    player_turn = True
 
-    elif choice == "stand":
-        while d_score < 17:
-            dealer_hand.append(deal_card())
-            d_score = calculate_score(dealer_hand)
-            print(f"rah my ahh got {d_score}")
+    while player_turn:
+        choice = input(f"\nwhat do yu want to do BOY (hit / stand): ").strip().lower()
+        if choice == "hit":
+            player_hand.append(deal_card())
+            p_score = calculate_score(player_hand)
+            print(f"You ahh got {p_score}")
 
-        if d_score > 21 or p_score > d_score:
-            print("aint no way this nigga gotta be cheating run ts back!" )
+            if p_score > 21:
+                print("I JUST BUSTED ALL OVER YOOOOOUUUUUU")
+                player_turn = False
+            elif p_score == 21:
+                print("u gotta be kavango or smth??? i refuse to believe u beat me")
+                player_turn = False
+
+        elif choice == "stand":
+            while d_score < 17:
+                dealer_hand.append(deal_card())
+                d_score = calculate_score(dealer_hand)
+                print(f"rah my ahh got {d_score}")
+
+            if d_score > 21 or p_score > d_score:
+                print("aint no way this nigga gotta be cheating run ts back!" )
+            elif p_score == d_score:
+                print("Push / Standoff!")
+            else:
+                print("I won lmaooooooo!")
             player_turn = False
-        elif p_score == d_score:
-            print("Push / Standoff!")
         else:
-            print("I won lmaooooooo!")
-        player_turn = False
-    else:
-        print("Hella man i said hit or stand mxm!")
+            print("Hella man i said hit or stand mxm!")
+
+
+    rematch = input("HELLA LETS RUN TS BACK FR (yes / no): ").strip().lower()
+    if rematch != "yes":
+        print("CMON U SCARED??")
+        play_again = False
+        
+
